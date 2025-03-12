8e513bb9d45f0f339a4ea552b235ec20ea3c1f43
matheusclima <matheuslima1607@gmail.com>
2025-03-12 19:29:57 -0300

    feat: Implement user management module with user creation functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //Habilitando CORS
+  const allowedOrigins = [process.env.URL_FRONTEND];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Access-Control-Allow-Headers',
+      'Authorization',
+    ],
+    exposedHeaders: ['Set-Cookie'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 5500);
 }
 bootstrap();
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm';
 
 @Entity({ name: 'users' })
-export class Usuario {
+export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
